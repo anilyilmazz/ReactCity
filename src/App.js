5d7f2fb7d392d55cd46cb8a82a864bdff71dcd7c
anil yilmaz <anil.yilmaz@sys.com.tr>
2020-04-27 15:42:43 +0300

    tour and tour list component create

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import "./App.scss";
 import Navbar from "./components/Navbar/Navbar";
+import TourList from "./components/TourList";
 
 class App extends Component {
   render() {
     return (<React.Fragment>
       <Navbar></Navbar>
+      <TourList></TourList>
       </React.Fragment>);
   }
 }
---
+++ src/components/TourList/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import Tour from '../Tour/Tour'
+import "./tourlist.scss"
+
+ class TourList extends Component {
+    render() {
+        return (
+            <div>
+               TourList 
+               <Tour></Tour>
+            </div>
+        )
+    }
+}
+
+export default TourList;
